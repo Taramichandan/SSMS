31d1d8704dbd24aecf26c5da32ac876421e3e870
chandan tarami <taramimagarchandan@gmail.com>
2026-04-13 08:06:26 +0545

    Creating choices in system

---
+++ main.py
@@ -0,0 +1,42 @@
+def menu():
+    print("\n =========== Smart Student Management System ==============")
+    print("1. Add Student")
+    print("2. Add Marks")
+    print("3. View Students")
+    print("4. Search Student")
+    print("5. Delete Student")
+    print("6. Exit")
+
+
+def main():
+    
+    while True :
+        menu()
+
+        choice = input("Enter the choice : ")
+
+        if choice == "1" :
+            pass
+
+        elif choice == "2" :
+            pass
+
+        elif choice == "3" :
+            pass
+
+        elif choice == "4" :
+            pass
+
+        elif choice == "5" :
+            pass
+
+        elif choice == "6" :
+            print("Existing System!!! ")
+            break
+
+        else : 
+            print("Invalid Choice!!! ")
+
+
+if __name__ == "__main__" :
+    main()
